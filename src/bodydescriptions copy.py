8cf50996856833f7157fdd2765ed1e2c27610c42
thearol <thearollesloth@hotmail.com>
2022-05-19 08:36:53 +0200

    issue

--- src/bodydescriptions copy.py
+++ src/bodydescriptions copy.py
@@ -4,29 +4,28 @@ import spacy
 from pathlib import Path
 import os
 import json
-from util_func import*
+from util_func import * #read_data, nouns_bodyparts, detect_owner, descriptive_words
 
 nlp = spacy.load("en_core_web_sm")
 nlp.add_pipe("sentencizer")
 
-metapath = "/Users/thearolskovsloth/gutenberg.nosync/metadata/meta_gender_en_12-05-202215:06:38.csv"
-all_data_path = "/Users/thearolskovsloth/gutenberg.nosync/data/text/" 
+
 
 def main():
-
+    
     # read gender identification file
-    with open("identify-gender.json","r") as fp:
+    with open(os.path.join("..", "meta","identify-gender.json"),"r") as fp:
         identifiable_gender = json.load(fp)
 
-    with open("author-genders.json","r") as fp:
+    with open(os.path.join("..", "meta","author-genders.json"),"r") as fp:
         author_genders = json.load(fp)
 
     # load dataframe containing bodyparts
-    body = pd.read_csv("bodyparts.csv")
+    body = pd.read_csv(os.path.join("..", "meta","bodyparts.csv"))
     bodywords = list(body["BodyPart"])
 
     # path to txt files
-    all_data_path = os.path.join(os.getcwd(), "data")
+    all_data_path = os.path.join( "..", "..", "gutenberg-data", "data")
 
     #read data
     texts, ids = read_data(all_data_path)
@@ -36,6 +35,7 @@ def main():
     main_df = pd.DataFrame(columns = column_names)
 
     for book, book_id in zip(texts,ids):
+        print(book_id)
         for chunk in nlp.pipe(book):
             for sentences in chunk.sents:
                 all_bodyparts = nouns_bodyparts(sentences, bodywords)
@@ -49,6 +49,7 @@ def main():
                         author_gender = author_genders[book_id]
                         main_df.loc[len(main_df.index)] = [bodypart, located_owner,owner_gender, description, ID, author_gender]
 
-    main_df.to_csv("/Users/thearolskovsloth/Documents/MASTERS_I_COGSCI/second_sem/DataScience/datascience_exam/GenderedDescriptions/dataatlast.csv")
+    main_df.to_csv(os.path.join("..","output", "body_descriptions.csv"))
 
-
+if __name__=="__main__":
+    main()
